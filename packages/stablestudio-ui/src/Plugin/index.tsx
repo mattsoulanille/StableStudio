ce8a7d0490ff23c8a52ce732adc135f2a3f6321a
Matthew Soulanille <matthew@soulanille.net>
2023-06-08 18:41:02 -0700

    Add webgpu plugin launch support to package.json

--- packages/stablestudio-ui/src/Plugin/index.tsx
+++ packages/stablestudio-ui/src/Plugin/index.tsx
@@ -2,6 +2,7 @@ import * as StableStudio from "@stability/stablestudio-plugin";
 import * as StableStudioPluginExample from "@stability/stablestudio-plugin-example";
 import * as StableStudioPluginStability from "@stability/stablestudio-plugin-stability";
 import * as StableStudioPluginWebUI from "@stability/stablestudio-plugin-webui";
+import * as StableStudioPluginWebGPU from "@stability/stablestudio-plugin-webgpu";
 
 import { Environment } from "~/Environment";
 import { Generation } from "~/Generation";
@@ -118,6 +119,8 @@ namespace State {
         ? StableStudioPluginExample
         : Environment.get("USE_WEBUI_PLUGIN") === "true"
         ? StableStudioPluginWebUI
+        : Environment.get("USE_WEBGPU_PLUGIN") === "true"
+        ? StableStudioPluginWebGPU
         : StableStudioPluginStability;
 
     return {
